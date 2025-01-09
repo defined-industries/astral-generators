978fe5698993242437a22ccd5e4971b163ece79f
Nguyen Minh Tho <definedentity@proton.me>
2025-01-09 14:00:26 +0700

    More ports

--- src/main/kotlin/dev/definedentity/astralgenerators/blocks/ports/AGPorts.kt
+++ src/main/kotlin/dev/definedentity/astralgenerators/blocks/ports/AGPorts.kt
@@ -17,42 +17,66 @@ import java.util.function.Supplier
 object AGPorts {
 
     val BOILER_INPUT_HATCH =
-        create("boiler_input_hatch", "boiler_casing", ::BoilerInputHatch).register()
+        createInput("boiler_input_hatch", "boiler_casing", PortType.FLUID, ::BoilerInputHatch).register()
     val BOILER_OUTPUT_HATCH =
-        create("boiler_output_hatch", "boiler_casing", ::BoilerOutputHatch, false).register()
+        createOutput("boiler_output_hatch", "boiler_casing", PortType.FLUID, ::BoilerOutputHatch).register()
 
     val FUSION_REACTOR_INPUT_HATCH =
-        create("fusion_reactor_input_hatch", "fusion_reactor_casing", ::BoilerInputHatch).register()
+        createInput(
+            "fusion_reactor_input_hatch",
+            "fusion_reactor_casing",
+            PortType.FLUID,
+            ::BoilerInputHatch
+        ).register()
     val FUSION_REACTOR_OUTPUT_HATCH =
-        create("fusion_reactor_output_hatch", "fusion_reactor_casing", ::BoilerInputHatch, false).register()
+        createOutput(
+            "fusion_reactor_output_hatch",
+            "fusion_reactor_casing",
+            PortType.FLUID,
+            ::BoilerInputHatch
+        ).register()
+    val FUSION_REACTOR_ENERGY_INPUT_HATCH =
+        createInput(
+            "fusion_reactor_energy_input_hatch",
+            "fusion_reactor_casing",
+            PortType.ENERGY,
+            ::BoilerInputHatch
+        ).register()
+    val FUSION_REACTOR_ENERGY_OUTPUT_HATCH =
+        createOutput(
+            "fusion_reactor_energy_output_hatch",
+            "fusion_reactor_casing",
+            PortType.ENERGY,
+            ::BoilerInputHatch
+        ).register()
+    val FUSION_REACTOR_INPUT_BUS =
+        createInput("fusion_reactor_input_bus", "fusion_reactor_casing", PortType.ITEM, ::BoilerInputHatch).register()
+    val FUSION_REACTOR_OUTPUT_BUS =
+        createOutput("fusion_reactor_output_bus", "fusion_reactor_casing", PortType.ITEM, ::BoilerInputHatch).register()
 
     val STEAM_TURBINE_INPUT_HATCH =
-        create("steam_turbine_input_hatch", "steam_turbine_casing", ::BoilerInputHatch).register()
+        createInput("steam_turbine_input_hatch", "steam_turbine_casing", PortType.FLUID, ::BoilerInputHatch).register()
     val STEAM_TURBINE_OUTPUT_HATCH =
-        create("steam_turbine_output_hatch", "steam_turbine_casing", ::BoilerInputHatch, false).register()
+        createOutput(
+            "steam_turbine_output_hatch",
+            "steam_turbine_casing",
+            PortType.FLUID,
+            ::BoilerInputHatch
+        ).register()
+    val STEAM_TURBINE_ENERGY_OUTPUT_HATCH =
+        createOutput(
+            "steam_turbine_energy_output_hatch",
+            "steam_turbine_casing",
+            PortType.ENERGY,
+            ::BoilerInputHatch
+        ).register()
 
-    /**
-     *
-     *     val STEAM_TURBINE_ENERGY_OUTPUT_HATCH =
-     *         register("boiler_energy_output_hatch", ::BoilerInputHatch).register()
-     *
-     *
-     *     val FUSION_REACTOR_INPUT_BUS =
-     *         register("fusion_reactor_input_bus", ::BoilerInputHatch).register()
-     *     val FUSION_REACTOR_OUTPUT_BUS =
-     *         register("fusion_reactor_output_bus", ::BoilerInputHatch).register()
-     *     val FUSION_REACTOR_ENERGY_INPUT_HATCH =
-     *         register("fusion_reactor_energy_input_hatch", ::BoilerInputHatch).register()
-     *     val FUSION_REACTOR_ENERGY_OUTPUT_HATCH =
-     *         register("fusion_reactor_energy_output_hatch", ::BoilerInputHatch).register()
-     */
-
-    private fun <T : Block> create(
+    private fun <T : Block> createInput(
         name: String,
         casingName: String,
         displayName: String,
+        type: PortType,
         blockSupplier: (BlockBehaviour.Properties) -> T,
-        isInput: Boolean = true
     ): BlockBuilder<T, Registrate> {
         return REGISTRATE.block(name, blockSupplier).lang(displayName)
             .properties {
@@ -67,19 +91,56 @@ object AGPorts {
                         .texture("bot_all", prov.modLoc("block/casings/${casingName}"))
                         .texture(
                             "top_all",
-                            prov.modLoc("block/overlays/overlay_fluid_${if (isInput) "input" else "output"}")
+                            prov.modLoc("block/overlays/overlay_${type}_input")
                         )
                 )
             }.simpleItem().addLayer { Supplier { RenderType.translucent() } }
     }
 
-    private fun <T : Block> create(
+    private fun <T : Block> createInput(
         name: String,
         casingName: String,
+        type: PortType,
         blockSupplier: (BlockBehaviour.Properties) -> T,
-        isInput: Boolean = true
-    ): BlockBuilder<Block, Registrate> {
-        return create(name, casingName, TextFormatting.toEnglishName(name), blockSupplier, isInput)
+
+        ): BlockBuilder<Block, Registrate> {
+        return createInput(name, casingName, TextFormatting.toEnglishName(name), type, blockSupplier)
+    }
+
+    private fun <T : Block> createOutput(
+        name: String,
+        casingName: String,
+        displayName: String,
+        type: PortType,
+        blockSupplier: (BlockBehaviour.Properties) -> T,
+    ): BlockBuilder<T, Registrate> {
+        return REGISTRATE.block(name, blockSupplier).lang(displayName)
+            .properties {
+                BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL)
+                    .requiresCorrectToolForDrops()
+                    .strength(1f, 1f)
+                    .sound(SoundType.METAL)
+            }.blockstate { ctx, prov ->
+                prov.simpleBlock(
+                    ctx.entry,
+                    prov.models().withExistingParent(ctx.name, prov.modLoc("block/cube_2_layer/all"))
+                        .texture("bot_all", prov.modLoc("block/casings/${casingName}"))
+                        .texture(
+                            "top_all",
+                            prov.modLoc("block/overlays/overlay_${type}_output")
+                        )
+                )
+            }.simpleItem().addLayer { Supplier { RenderType.translucent() } }
+    }
+
+    private fun <T : Block> createOutput(
+        name: String,
+        casingName: String,
+        type: PortType,
+        blockSupplier: (BlockBehaviour.Properties) -> T,
+
+        ): BlockBuilder<Block, Registrate> {
+        return createOutput(name, casingName, TextFormatting.toEnglishName(name), type, blockSupplier)
     }
 
     fun init() {}
